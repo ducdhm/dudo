da9e2fd4e308ece998a68d644d2c57c52c4094ca
Duc Doan Hoang Minh (IT - ONL) <ducdhm@vpbank.com.vn>
2019-02-19 09:30:38 +0700

    Minify HTML on prod only

--- local_modules/meen/core/minify.js
+++ local_modules/meen/core/minify.js
@@ -1,17 +1,20 @@
 // Minify HTML on response
 // --------------------------------
 const minifyHTML = require('express-minify-html');
+const env = require('../utils/env');
 module.exports = (app) => {
-    app.use(minifyHTML({
-        override: true,
-        exception_url: false,
-        htmlMinifier: {
-            removeComments: true,
-            collapseWhitespace: true,
-            collapseBooleanAttributes: true,
-            removeAttributeQuotes: true,
-            removeEmptyAttributes: true,
-            minifyJS: true
-        }
-    }));
+    if (env.isProd) {
+        app.use(minifyHTML({
+            override: true,
+            exception_url: false,
+            htmlMinifier: {
+                removeComments: true,
+                collapseWhitespace: true,
+                collapseBooleanAttributes: true,
+                removeAttributeQuotes: true,
+                removeEmptyAttributes: true,
+                minifyJS: true
+            }
+        }));
+    }
 };
