3a4b15f2a80b53d201610e50ec985d866d4e7859
Duc Doan Hoang Minh (IT - ONL) <ducdhm@vpbank.com.vn>
2019-02-25 15:14:54 +0700

    Common config

--- config/dev.js
+++ config/dev.js
@@ -1,32 +1,8 @@
-module.exports = {
-    app: {
-        title: 'M.E.E.N CMS',
-        version: '1.0.0'
-    },
+const deepExtend = require('deep-extend');
+const commonConfig = require('./common');
+
+module.exports = deepExtend(commonConfig, {
     mongodb: {
-        url: 'mongodb://localhost/meen-dev',
-        options: {
-            useNewUrlParser: true
-        }
-    },
-    cronjob: {
-        timezone: 'Asia/Ho_Chi_Minh'
-    },
-    adminPort: 5000,
-    apiPort: 7000,
-    session: {
-        secret: 'meen'
-    },
-    bcrypt: {
-        salt: 10
-    },
-    uploadPath: {
-        image: 'public/uploads/image/'
-    },
-    jwt: {
-        secret: 'meen',
-        expires: 60 * 60 * 24
-    },
-    itemPerPage: 10,
-    logFile: false
-};
+        url: 'mongodb://localhost/meen-dev'
+    }
+});
