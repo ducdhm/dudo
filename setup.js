d2b2386b514b5282a278bd48680ae0152122d8ab
Duc Doan Hoang Minh (IT - ONL) <ducdhm@vpbank.com.vn>
2019-03-13 16:14:24 +0700

    Refactor part 8

--- setup.js
+++ setup.js
@@ -2,9 +2,11 @@ const {
     composeApp,
     mongoose
 } = require('meen');
-const app = composeApp('setup', [
-    mongoose
-]);
+const app = composeApp('setup',
+    [
+        mongoose
+    ]
+);
 
 const logger = app.logger('setup');
 const UserModel = require('models/User');
@@ -15,7 +17,7 @@ const bcrypt = require('bcryptjs');
         let admin = await UserModel.findOne({
             query: {username: 'god'}
         });
-
+        
         admin = admin || UserModel.create();
         await UserModel.save(admin, {}, populateAdmin);
         logger.info('Setup admin is done!');
@@ -31,6 +33,6 @@ function populateAdmin(admin) {
     admin.god = true;
     admin.username = 'god';
     admin.password = bcrypt.hashSync('Meen@2018');
-
+    
     return admin;
 }
