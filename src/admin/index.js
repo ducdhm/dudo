423a5c49ddd4e0ad323b17d4ddb03b05fd2a7cb9
Duc Doan Hoang Minh (IT - ONL) <ducdhm@vpbank.com.vn>
2019-04-25 17:32:40 +0700

    Integrate Limitless part 2

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,12 +1,31 @@
-// Plugins
+// Core scripts
 // -----------------------------------------------
 import 'jquery';
-
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import 'font-awesome/scss/font-awesome.scss';
-
 import 'jquery.niceform';
 import 'bootstrap-msg';
+
+// Core styles
+// -----------------------------------------------
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/scss/font-awesome.scss';
+import 'bootstrap-msg/dist/css/bootstrap-msg.css';
+
+
+// Theme scripts
+// -----------------------------------------------
+
+
+// Theme styles
+// -----------------------------------------------
+import '../fonts/Roboto.scss';
+import '../limitless-2.0.1/global_assets/css/icons/icomoon/styles.css';
+import '../limitless-2.0.1/layout_2/'
+
+// <link href="../../../../global_assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+//     <link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+//     <link href="assets/css/bootstrap_limitless.min.css" rel="stylesheet" type="text/css">
+//     <link href="assets/css/layout.min.css" rel="stylesheet" type="text/css">
+//     <link href="assets/css/components.min.css" rel="stylesheet" type="text/css">
+//     <link href="assets/css/colors.min.css" rel="stylesheet" type="text/css">
 
