043b106239829e917d63d2f108ad74d490be28a5
ducdhm <ducdhm@gmail.com>
2019-05-03 00:08:38 +0700

    Integrate Limitless part 5

--- src/admin/login.js
+++ src/admin/login.js
@@ -1,10 +1,10 @@
-import $ from 'jquery';
 import './styles/login.scss';
+import 'jquery.niceform';
 
 $(function () {
-    // $('form').niceform({
-    //     onAjaxSuccess: function (resp) {
-    //         window.location.href = resp.data.returnTo;
-    //     }
-    // });
+    $('form').niceform({
+        onAjaxSuccess: function (resp) {
+            window.location.href = resp.data.returnTo;
+        }
+    });
 });
