2d1f251f3606e86f14124d6f6269f76b646b956b
Duc Doan <ducdhm@gmail.com>
2019-05-27 14:32:34 +0700

    Integrate Limitless

--- config/icon.js
+++ config/icon.js
@@ -1,24 +1,26 @@
 module.exports = {
     DASHBOARD: 'icon-home4',
+    
     USER: 'icon-user',
     USER_PROFILE: 'icon-user-tie',
     USERS: 'icon-users2',
-    IMAGE: 'icon-images2',
     PASSWORD: 'icon-lock2',
     LOGOUT: 'icon-switch2',
+    
     ADD: 'icon-plus22',
     EDIT: 'icon-pencil7',
     REMOVE: 'icon-trash',
-    APPROVED: 'fa fa-check',
-    REJECTED: 'fa fa-times',
     SAVE: 'fa fa-save',
     BACK: 'fa fa-chevron-left',
-    CHECKED: 'fa fa-check',
-    REVIEW: 'fa fa-eye',
-    ERROR: 'fa fa-warning',
-    SEARCH: 'fa fa-search-plus',
-    SELECT_IMAGE: 'fa fa-check',
+    
+    CHECKED: 'icon-checkmark3',
+    SEARCH: 'icon-search4',
+    
+    SELECT_IMAGE: 'icon-checkmark3',
     CROP_IMAGE: 'icon-crop2',
-    TAG: 'fa fa-tags',
-    NLP: 'icon-yin-yang'
+    IMAGE: 'icon-images2',
+    
+    ERROR: 'icon-warning',
+    
+    NLP: 'icon-yin-yang',
 };
