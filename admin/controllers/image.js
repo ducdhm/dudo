0e31a3013ae0995bf9ee96a681bbede4264d6c16
Duc Doan <ducdhm@gmail.com>
2019-05-28 17:30:45 +0700

    Integrate Limitless - Images

--- admin/controllers/image.js
+++ admin/controllers/image.js
@@ -16,17 +16,22 @@ module.exports = (app) => {
     const upload = multer({storage});
     
     const imageController = {};
-    imageController.getImages = (req, res, next) => {
-        fs.readdir(config.uploadPath.image, (error, items) => {
-            if (error) {
-                return next(error);
-            }
-            
+    imageController.getImages = async (req, res, next) => {
+        try {
+            let items = fs.readdirSync(config.uploadPath.image);
+            items = items.map(item => {
+                // let stat = fs.statSync(config.uploadPath.image + item);
+                
+                return item;
+            });
+    
             return res.render('image', {
                 title: req.query.hasOwnProperty('iframe') ? 'Images list' : 'Manage images',
                 files: items
             });
-        })
+        } catch (error) {
+            return next(error);
+        }
     };
     
     imageController.imageUpload = [
@@ -35,16 +40,16 @@ module.exports = (app) => {
         (req, res, next) => res.status(200).send(req.file)
     ];
     
-    imageController.imageDelete = (req, res, next) => {
-        fs.unlink(config.uploadPath.image + req.body.file, (error) => {
-            if (error) {
-                return errorHandlers.jsonError(error, res);
-            }
-            
+    imageController.imageDelete = async (req, res, next) => {
+        try {
+            fs.unlinkSync(config.uploadPath.image + req.body.file);
+    
             res.json({
                 status: true
             });
-        });
+        } catch (error) {
+            return errorHandlers.jsonError(error, res);
+        }
     };
     
     return imageController;
