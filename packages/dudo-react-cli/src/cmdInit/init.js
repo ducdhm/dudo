0bdfc1b4d1236ab131c5358dbc6d4b4be398752b
Duc Doan <ducdhm@gmail.com>
2021-04-30 14:56:04 +0700

    Init structure from "template" folder

--- packages/dudo-react-cli/src/cmdInit/init.js
+++ packages/dudo-react-cli/src/cmdInit/init.js
@@ -16,68 +16,15 @@ module.exports = (logger) => (target, options) => {
     }
 
     logger.info(`Creating structure...`);
-    const structure = {
-        'public': {
-            'static': {
-                'media': {
-
-                },
-            },
-            'index.html': path.join(__dirname, './templates/public/index.html.hbs'),
-            'manifest.json': path.join(__dirname, './templates/public/manifest.json.hbs'),
-            'robots.txt': path.join(__dirname, './templates/public/robots.txt.hbs'),
+    initStructure({
+        target: targetPath,
+        structure: path.join(__dirname, './template'),
+        fileData: {
+            name: options.name || 'dreact',
+            title: options.title || 'Dreact App',            
+            description: options.description || 'This app is created by using "@dudojs/react-cli"',        
+            color: options.color || '#000000',        
         },
-        'src': {
-            'app': {
-                'App.js': path.join(__dirname, './templates/src/app/App.js.hbs'),
-                'App.scss': path.join(__dirname, './templates/src/app/App.scss.hbs'),
-            },
-            'assets': {
-                'fonts': {
-
-                },
-                'images': {
-
-                },
-                'styles': {
-                    '_mixins.scss': path.join(__dirname, './templates/src/assets/styles/_mixins.scss.hbs'),
-                    '_variables.scss': path.join(__dirname, './templates/src/assets/styles/_variables.scss.hbs'),
-                },
-            },
-            'components': {
-
-            },
-            'helpers': {
-
-            },
-            'layouts': {
-
-            },
-            'redux': {
-
-            },
-            'services': {
-
-            },
-            'views': {
-
-            },
-            'index.js': path.join(__dirname, './templates/src/index.js.hbs'),
-        },
-        '.babelrc': path.join(__dirname, './templates/.babelrc.hbs'),
-        '.env.development': path.join(__dirname, './templates/.env.development.hbs'),
-        '.env.production': path.join(__dirname, './templates/.env.production.hbs'),
-        '.env.staging': path.join(__dirname, './templates/.env.staging.hbs'),
-        '.gitignore': path.join(__dirname, './templates/.gitignore.hbs'),
-        'README.md': path.join(__dirname, './templates/README.md.hbs'),
-        'package.json': path.join(__dirname, './templates/package.json.hbs'),
-    };
-
-    initStructure(targetPath, structure, {
-        name: options.name || 'dreact',
-        title: options.title || 'Dreact App',            
-        description: options.description || 'This app is created by using "@dudojs/react-cli"',        
-        color: options.color || '#000000',        
     });
     
     logger.info(`Installing npm depencencies...`);
