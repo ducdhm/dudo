fd3cfbbb3cf468f6e8813e20ecbea69bc02ab8bf
Duc Doan <ducdhm@gmail.com>
2021-08-19 10:17:24 +0700

    openPopup

--- packages/dudo-browser-utils/index.js
+++ packages/dudo-browser-utils/index.js
@@ -2,4 +2,5 @@ export { dataUriToBlob } from './src/dataUriToBlob';
 export { getBreakPoint } from './src/getBreakPoint';
 export { getOffset } from './src/getOffset';
 export { getScrollbarWidth } from './src/getScrollbarWidth';
+export { openPopup } from './src/openPopup';
 export { scrollTo } from './src/scrollTo';
