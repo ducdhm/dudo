7e8fc43469167d2145995cdd22c397fa0dca28df
Duc Doan <ducdhm@gmail.com>
2021-08-19 16:01:58 +0700

    Remove logger when generating index

--- packages/dudo-utils/tools/genIndex/index.js
+++ packages/dudo-utils/tools/genIndex/index.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const logger = require('@dudojs/logger')();
 const updateIndex = require('update-index');
 
-logger.info(`Generate "index.js" file`);
 updateIndex(path.join(__dirname, '..', '..', 'src'), path.join(__dirname, '..', '..'), 'FILE');
