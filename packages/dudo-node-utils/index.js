282ba8d437ef9409b662357278a10ae179f2951a
Duc Doan <ducdhm@gmail.com>
2021-09-28 16:13:59 +0700

    Remove getWinstonLogger

--- packages/dudo-node-utils/index.js
+++ packages/dudo-node-utils/index.js
@@ -3,7 +3,6 @@ module.exports = {
     getExcelSheetData: require('./src/getExcelSheetData'),
     getFileList: require('./src/getFileList'),
     getFolderList: require('./src/getFolderList'),
-    getWinstonLogger: require('./src/getWinstonLogger'),
     resolvePath: require('./src/resolvePath'),
     writeExcel: require('./src/writeExcel'),
     writeExcelToFile: require('./src/writeExcelToFile'),
