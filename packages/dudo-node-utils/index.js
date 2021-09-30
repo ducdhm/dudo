c6c774479c88c8ea2c8f3e01894b322093ac4c71
Duc Doan <ducdhm@gmail.com>
2021-09-30 13:32:17 +0700

    Update

--- packages/dudo-node-utils/index.js
+++ packages/dudo-node-utils/index.js
@@ -3,6 +3,7 @@ module.exports = {
     getExcelSheetData: require('./src/getExcelSheetData'),
     getFileList: require('./src/getFileList'),
     getFolderList: require('./src/getFolderList'),
+    getWinstonLogger: require('./src/getWinstonLogger'),
     resolvePath: require('./src/resolvePath'),
     writeExcel: require('./src/writeExcel'),
     writeExcelToFile: require('./src/writeExcelToFile'),
