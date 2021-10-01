782332e4c0ca1e86510e4eec8dc0670abd10ae56
Duc Doan <ducdhm@gmail.com>
2021-10-01 15:41:20 +0700

    No need for clear and figlet

--- packages/dudo-auto-deploy-cli/index.js
+++ packages/dudo-auto-deploy-cli/index.js
@@ -2,28 +2,6 @@
 
 const packageJson = require('./package');
 const program = require('commander');
-const clear = require('clear');
-const figlet = require('figlet');
-const color = require('colors/safe');
-
-const printBanner = () => {
-    clear();
-    console.log(
-        color.blue(
-            '===============================[ (c) Duc Doan ]==============================='
-            +
-            '\n'
-            +
-            figlet.textSync('Auto Deploy', {
-                horizontalLayout: 'full',
-            })
-            +
-            '\n'
-            +
-            '=============================[ ducdhm@gmail.com ]============================='
-        )
-    );
-};
 
 
 // Setup program
@@ -48,13 +26,6 @@ program.on('command:*', () => {
     console.error('Invalid command: %s\nSee --help for a list of available commands.', program.args.join(' '));
     process.exit(1);
 });
-
-
-// Print banner if printing help
-// -----------------------------------------
-if (process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-    printBanner();
-}
 
 
 // When no command
