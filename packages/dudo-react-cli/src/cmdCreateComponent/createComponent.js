ff80fdaf409014ff4c029b3625927c4982d66ea7
Duc Doan <ducdhm@gmail.com>
2022-01-27 16:15:44 +0700

    Simplify component files name, support component of page

--- packages/dudo-react-cli/src/cmdCreateComponent/createComponent.js
+++ packages/dudo-react-cli/src/cmdCreateComponent/createComponent.js
@@ -3,28 +3,44 @@ const initStructure = require('@dudojs/init-structure');
 const path = require('path');
 
 module.exports = (name, options) => {
-    const { page, layout } = options;
+    const { page } = options;
     logger.info(`Create "${name}" component`);
 
+    const arr = name.replace(/\\/g, '/').split('/');
+
+    let componentName = name;
     let componentFolder = 'components';
+
     if (page) {
         componentFolder = 'pages';
     }
-    if (layout) {
-        componentFolder = 'layouts';
+
+    if (arr.length > 1) {
+        componentName = arr[1];
+        componentFolder = 'pages';
     }
 
     const structure = {};
-    structure[name] = {};
-    structure[name][`index.js`] = path.join(__dirname, './template/component.index.hbs');
-    structure[name][`${name}.component.js`] = path.join(__dirname, './template/component.component.hbs');
-    structure[name][`${name}.style.scss`] = path.join(__dirname, './template/component.style.hbs');
+    if (arr.length === 1) {
+        structure[componentName] = {};
+        structure[componentName][`index.js`] = path.join(__dirname, './template/component.index.hbs');
+        structure[componentName][`${componentName}.js`] = path.join(__dirname, './template/component.js.hbs');
+        structure[componentName][`${componentName}.scss`] = path.join(__dirname, './template/component.scss.hbs');
+    } else {
+        const pageName = arr[0];
+        componentName = arr[1];
+        structure[pageName] = {};
+        structure[pageName][componentName] = {};
+        structure[pageName][componentName][`index.js`] = path.join(__dirname, './template/component.index.hbs');
+        structure[pageName][componentName][`${componentName}.js`] = path.join(__dirname, './template/component.js.hbs');
+        structure[pageName][componentName][`${componentName}.scss`] = path.join(__dirname, './template/component.scss.hbs');
+    }
 
     initStructure({
         target: getTargetPath(`src/${componentFolder}`),
         structure,
         fileData: {
-            name,
+            name: componentName,
         },
     });
 
