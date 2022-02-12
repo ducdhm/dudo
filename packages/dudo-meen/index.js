8ccda2f700e51d7f20af1b8facd3a14408cc4982
Duc Doan <ducdhm@gmail.com>
2022-02-13 00:39:52 +0700

    Remove "composeModel"

--- packages/dudo-meen/index.js
+++ packages/dudo-meen/index.js
@@ -1,5 +1,4 @@
 module.exports = {
     composeApp: require('./src/composeApp'),
-    composeModel: require('./src/composeModel'),
     modules: require('./src/modules'),
 };
--- packages/dudo-meen/src/composeModel/composeUrlVirtual.js
+++
@@ -1,9 +0,0 @@
-const { toKebabCase } = require('@dudojs/utils');
-
-module.exports = (modelName, modelSchema) => {
-    const url = toKebabCase(modelName);
-
-    modelSchema.virtual('url').get(function () {
-        return `/${url}/${this._id}`;
-    });
-};
