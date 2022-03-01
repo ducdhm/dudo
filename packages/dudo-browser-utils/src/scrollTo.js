0336d5b28f529becf4a30f76c5db8c9ffb4b0204
Duc Doan <ducdhm@gmail.com>
2022-03-01 12:50:58 +0700

    Update "scrollTo" method

--- packages/dudo-browser-utils/src/scrollTo.js
+++ packages/dudo-browser-utils/src/scrollTo.js
@@ -1,6 +1,6 @@
 import getOffset from './getOffset';
 
-export const scrollTo = (element, duration = 200, offset = 0) => {
+export const scrollTo = (element, offset = 0) => {
     let elementY = getOffset(element).top + offset;
 
     window.scrollTo({ top: elementY, behavior: 'smooth' });
