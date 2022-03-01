c3d388bfd67c9c27805a76b42e50c30baae32d1f
Duc Doan <ducdhm@gmail.com>
2022-03-01 12:52:31 +0700

    Remove scrollIntoView

--- packages/dudo-browser-utils/src/scrollIntoView.js
+++
@@ -1,9 +0,0 @@
-export const scrollIntoView = (el, options) => {
-    if (typeof el === 'string') {
-        el = document.querySelector(el);
-    }
-
-    el.scrollIntoView(options || { behavior: 'smooth', block: 'start' });
-};
-
-export default scrollIntoView;
