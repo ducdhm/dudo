a3da7fd8d28ab0dd4e42ade56080532389553ed3
Duc Doan <ducdhm@gmail.com>
2022-05-12 00:10:10 +0700

    Support "-t --target" for component command

--- packages/dudo-react-cli/src/cmdCreateComponent/index.js
+++ packages/dudo-react-cli/src/cmdCreateComponent/index.js
@@ -2,6 +2,7 @@ module.exports = (program) => {
     program
         .command('component <name>')
         .description('create component')
+        .option('-t, --target <target>', 'target folder')
         .option('-l, --layout', 'layout component')
         .option('-p, --page', 'page component')
         .action(require('./createComponent'));
