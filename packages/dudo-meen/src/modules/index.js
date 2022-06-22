8ddd1f4b416937eb6809c361a27f7f0822bc994f
Duc Doan <ducdhm@gmail.com>
2022-06-22 14:40:15 +0700

    Update

--- packages/dudo-meen/src/modules/index.js
+++ packages/dudo-meen/src/modules/index.js
@@ -1,12 +1,1 @@
-module.exports = {
-    bodyParser: require('./bodyParser'),
-    bruteForce: require('./bruteForce'),
-    compression: require('./compression'),
-    cors: require('./cors'),
-    handleError: require('./handleError'),
-    mongoose: require('./mongoose'),
-    morgan: require('./morgan'),
-    publicFolder: require('./publicFolder'),
-    session: require('./session'),
-    view: require('./view'),
-};
+module.exports = {    bodyParser: require('./bodyParser'),    bruteForce: require('./bruteForce'),    compression: require('./compression'),    cors: require('./cors'),    handleError: require('./handleError'),    mongoose: require('./mongoose'),    morgan: require('./morgan'),    publicFolder: require('./publicFolder'),    session: require('./session'),    view: require('./view'),}
