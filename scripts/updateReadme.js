f429cb867a66fcef70a62c1956a3cd059410db3a
Duc Doan <ducdhm@gmail.com>
2023-06-29 22:47:46 +0700

    Format source-code for scripts

--- scripts/updateReadme.js
+++ scripts/updateReadme.js
@@ -1,26 +1,40 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
 const getPackageListText = (folderName) => {
-    const packageFolderPath = path.join(__dirname, '../' + folderName);
-    const packageFolderList = fs.readdirSync(packageFolderPath);
-    let result = '';
-    for (let folder of packageFolderList) {
-        const packagePath = path.join(packageFolderPath, folder, 'package.json');
-        const packageData = require(packagePath);
-        const isTypeScript = packageData.devDependencies && !!packageData.devDependencies.typescript;
+  const packageFolderPath = path.join(__dirname, '../' + folderName)
+  const packageFolderList = fs.readdirSync(packageFolderPath)
+  let result = ''
+  for (let folder of packageFolderList) {
+    const packagePath = path.join(packageFolderPath, folder, 'package.json')
+    const packageData = require(packagePath)
+    const isTypeScript = packageData.devDependencies && !!packageData.devDependencies.typescript
 
-        result += `* [${packageData.name}](./${folderName}/${folder}): ${packageData.description}${isTypeScript ? ' (TypeScript)' : ''}\n`;
-    }
+    result += `* [${packageData.name}](./${folderName}/${folder}): ${packageData.description}${isTypeScript ? ' (TypeScript)' : ''}\n`
+  }
 
-    return result;
-};
+  return result
+}
 
-const packageListText = getPackageListText('packages');
-const deprecatedListText = getPackageListText('deprecated');
+const packageListText = getPackageListText('packages')
+const deprecatedListText = getPackageListText('deprecated')
 
 fs.writeFileSync(
-    path.join(__dirname, '../README.md'),
-    `# DudoJS\nStack of awesome!\n\n\n## Packages\n${packageListText}\n\n## DEPRECATED\n${deprecatedListText}\n\n## License\nPlease read at [here](./LICENSE.md)\n`,
-    'utf8',
-);
+  path.join(__dirname, '../README.md'),
+  [
+    '# DudoJS',
+    '',
+    '',
+    'Stack of awesome!',
+    '',
+    '## Packages',
+    packageListText,
+    '',
+    '## DEPRECATED',
+    deprecatedListText,
+    '',
+    '## License',
+    'Please read at [here](./LICENSE.md)',
+  ].join('\n'),
+  'utf8',
+)
