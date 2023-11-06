7b9f47be5a12abe5650e93c6c99751b315dff878
Duc Doan <ducdhm@gmail.com>
2023-11-06 13:14:42 +0700

    Update logic

--- packages/dudo-json-merger/index.js
+++ packages/dudo-json-merger/index.js
@@ -1,21 +1,34 @@
 #!/usr/bin/env node
 
-const fs = require('fs')
-const path = require('path')
-const yargs = require('yargs')
-const { hideBin } = require('yargs/helpers')
-const { globSync } = require('glob')
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+import { globSync } from 'glob'
+import * as fs from 'fs'
+import * as path from 'path'
 
-;(() => {
+
+(() => {
   const argv = yargs(hideBin(process.argv)).argv
   const jsonFiles = globSync(argv.input || '**.json', { ignore: argv.ignore })
   const result = {}
+  let adIdList = []
 
   for (let file of jsonFiles) {
     const filePath = path.join(process.cwd(), file)
     const fileContent = fs.readFileSync(filePath, 'utf-8')
-    result[file] = JSON.parse(fileContent)
+    const fileJson = JSON.parse(fileContent)
+    adIdList = [
+      ...adIdList,
+      ...fileJson.results.map(item => {
+        return item.adGroupAd.ad.id
+      }),
+    ]
+    result[file] = fileJson
   }
+
+  const duplicatedIdList = adIdList.filter((item, index) => adIdList.indexOf(item) !== index)
+
+  console.log('Duplicated ad: ', duplicatedIdList)
 
   fs.writeFileSync(
     path.join(process.cwd(), argv.output || 'merged.json'),
